456cb7418c5c0ce39211a957cd7d74c1adebaafe
AdamJC20 <adamcummings20@googlemail.com>
2025-11-18 17:33:53 +0000

    transforming of GoG data finished

--- source/pipeline/transform.py
+++ source/pipeline/transform.py
@@ -7,9 +7,9 @@ from datetime import datetime
 
 
 DIRECTORY = 'data/'
-INPUT_PATH = f'{DIRECTORY}a_file.json'
+# INPUT_PATH = f'{DIRECTORY}a_file.json'
 INPUT_PATH = 'products.json'
-OUTPUT_PATH = f'{DIRECTORY}clean_data.csv'
+OUTPUT_PATH = f'{DIRECTORY}clean_data.json'
 # list dict, product id, name, base price, retail, final price
 
 # game
@@ -18,17 +18,50 @@ OUTPUT_PATH = f'{DIRECTORY}clean_data.csv'
 # price_record - + all data + current date
 
 
-def divide_folders(file: str) -> pd.DataFrame:
+def transform_gog(file: str) -> pd.DataFrame:
     """Used to get data from a nested dict. Will return None if any level isn't valid"""
+    with open(file) as f:
+        gog_data = json.load(f)
 
-    price_record_df = pd.DataFrame()
-    games = pd.DataFrame()
-    platforms = pd.DataFrame()
-    listings = pd.DataFrame()
+    all_data = pd.DataFrame(gog_data)
+
+    all_data.dropna(subset=['base_price_gbp_pence'], inplace=True)
+
+    valid = (all_data["base_price_gbp_pence"] > 0) & \
+            (all_data["final_price_gbp_pence"] > 0)
+
+    all_data["discount_percent"] = (
+        (1 - all_data["final_price_gbp_pence"] /
+         all_data["base_price_gbp_pence"]) * 100
+    ).where(valid).round().astype("Int64")
+
+    all_data.loc[:, 'discount_percent'] = all_data['discount_percent'].fillna(
+        0)
+
+    all_data = all_data[['name', 'base_price_gbp_pence',
+                         'final_price_gbp_pence', 'discount_percent']].copy()
+
+    all_data.loc[:, 'platform_name'] = 'gog'
+
+    all_data.loc[:, 'listing_date'] = datetime.today().date()
+
+    all_data['base_price_gbp_pence'] = all_data['base_price_gbp_pence'].astype(
+        'Int64')
+
+    all_data['final_price_gbp_pence'] = all_data['final_price_gbp_pence'].astype(
+        'Int64')
+
+    all_data['listing_date'] = all_data['listing_date'].astype('string')
+
+    all_data.rename(columns={'name': 'game_name',
+                    'base_price_gbp_pence': 'retail_price', 'final_price_gbp_pence': 'final_price'}, inplace=True)
+
+    all_data = all_data[['game_name', 'retail_price', 'platform_name',
+                         'listing_date', 'discount_percent', 'final_price']].copy()
+
+    return all_data
 
 
 if __name__ == "__main__":
-    with open(INPUT_PATH) as f:
-        gog_data = json.load(f)
-
-    input(divide_folders(INPUT_PATH))
+    cleaned_data = transform_gog(INPUT_PATH)
+    cleaned_data.to_json(OUTPUT_PATH, indent=4, orient='records')

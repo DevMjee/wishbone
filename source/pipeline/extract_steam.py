e7669999f1dba3dc3a03601ebff61eb06a48b242
DevMjee <dev.mjee2@gmail.com>
2025-11-18 10:59:58 +0000

    adding api which pull gb price in pennies

--- source/pipeline/extract_steam.py
+++ source/pipeline/extract_steam.py
@@ -1,5 +1,9 @@
 """Script which gets data from steam web api"""
-"""change"""
+URL = "https://store.steampowered.com/api/appdetails/?appids=1676840&cc=uk&filters=price_overview"
+
+
+def get_from_steam_db() -> None:
+    """Pull games, ids, and prices from database"""
 
 
 if __name__ == "__main__":

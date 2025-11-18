d43975c732d08d188e68029463dd0b016a7c2953
Annie <bulloughs@icloud.com>
2025-11-18 14:24:04 +0000

    Upadted schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,45 +1,43 @@
 IF EXISTS DROP DATABASE wishbone;
 CREATE DATABASE wishbone;
 
-\c wishbone;
-
 CREATE TABLE game (
-    game_id INT GENERATED ALWAYS AS IDENTITY,
+    game_id INT GENERATED ALWAYS AS identity (MINVALUE 1 START WITH 1 INCREMENT BY 1),
     game_name TEXT NOT NULL,
     retail_price INT NOT NULL,
     PRIMARY KEY(game_id)
 );
 
 CREATE TABLE tracking (
-    tracking_id INT GENERATED ALWAYS AS IDENTITY,
+    tracking_id INT GENERATED ALWAYS AS identity (MINVALUE 1 START WITH 1 INCREMENT BY 1),
     email TEXT NOT NULL,
-    game_id NOT NULL,
+    game_id INT NOT NULL,
     PRIMARY KEY(tracking_id),
     FOREIGN KEY (game_id) REFERENCES game(game_id)
 );
 
 CREATE TABLE platform(
-    platform_id INT GENERATED ALWAYS AS IDENTITY,
+    platform_id INT GENERATED ALWAYS AS identity (MINVALUE 1 START WITH 1 INCREMENT BY 1),
     platform_name TEXT NOT NULL,
     PRIMARY KEY(platform_id)
 );
 
 CREATE TABLE listing(
-    listing_id INT GENERATED ALWAYS AS IDENTITY,
+    listing_id INT GENERATED ALWAYS AS identity (MINVALUE 1 START WITH 1 INCREMENT BY 1),
     platform_code TEXT NOT NULL,
     game_id INT NOT NULL,
     platform_id INT NOT NULL,
     PRIMARY KEY(listing_id),
-    FOREIGN KEY (game_id) REFERENCES game(game_id)
+    FOREIGN KEY (game_id) REFERENCES game(game_id),
     FOREIGN KEY (platform_id) REFERENCES platform(platform_id)
 );
 
 CREATE TABLE price_record(
-    price_record_id INT GENERATED ALWAYS AS IDENTITY,
+    price_record_id INT GENERATED ALWAYS AS identity (MINVALUE 1 START WITH 1 INCREMENT BY 1),
     listing_id INT NOT NULL,
     price INT NOT NULL,
     discount_percent INT,
-    recording_date DATE CHECK recording_date <= CURRENT_DATE,
-    PRIMARY KEY(price_record_id)
+    recording_date DATE CHECK (recording_date <= CURRENT_DATE),
+    PRIMARY KEY(price_record_id),
     FOREIGN KEY (listing_id) REFERENCES listing(listing_id)
 );

ae1d2497f840cdb4a16d194cb6363d6fb203d122
AdamJC20 <adamcummings20@googlemail.com>
2025-11-19 12:09:20 +0000

    added comments to each step

--- source/pipeline/transform.py
+++ source/pipeline/transform.py
@@ -7,52 +7,57 @@ from datetime import datetime
 
 
 DIRECTORY = 'data/'
-# INPUT_PATH = f'{DIRECTORY}a_file.json'
-INPUT_PATH = 'products.json'
+INPUT_PATH = f'{DIRECTORY}products.json'
 OUTPUT_PATH = f'{DIRECTORY}clean_data.json'
-# list dict, product id, name, base price, retail, final price
-
-# game
-# listing - + GoG_id
-# platform - + Gog
-# price_record - + all data + current date
 
 
 def transform_gog(file: str) -> pd.DataFrame:
     """Used to get data from a nested dict. Will return None if any level isn't valid"""
+    # Read data from file
     with open(file) as f:
         gog_data = json.load(f)
 
+    # Create dataframe
     all_data = pd.DataFrame(gog_data)
 
+    # Drop NaN values
     all_data.dropna(subset=['base_price_gbp_pence'], inplace=True)
 
+    # Create boolean series to check which values to calculate discount for (avoid dividing by 0)
     valid = (all_data["base_price_gbp_pence"] > 0) & \
             (all_data["final_price_gbp_pence"] > 0)
 
+    # Calculate discount percentage
     all_data["discount_percent"] = (
         (1 - all_data["final_price_gbp_pence"] /
          all_data["base_price_gbp_pence"]) * 100
     ).where(valid).round().astype("Int64")
 
+    # Fill NaN values in discount percentage with zero
     all_data.loc[:, 'discount_percent'] = all_data['discount_percent'].fillna(
         0)
 
+    # Redefine dataframe with relevant columns
     all_data = all_data[['name', 'base_price_gbp_pence',
                          'final_price_gbp_pence', 'discount_percent']].copy()
 
+    # Set platform name for all rows
     all_data.loc[:, 'platform_name'] = 'gog'
 
+    # Timestamp data with today's date
     all_data.loc[:, 'listing_date'] = datetime.today().date()
 
+    # Cast prices to integers
     all_data['base_price_gbp_pence'] = all_data['base_price_gbp_pence'].astype(
         'Int64')
 
     all_data['final_price_gbp_pence'] = all_data['final_price_gbp_pence'].astype(
         'Int64')
 
+    # Cast listing date to string
     all_data['listing_date'] = all_data['listing_date'].astype('string')
 
+    # Rename and reorder columns to be in line with load script input
     all_data.rename(columns={'name': 'game_name',
                     'base_price_gbp_pence': 'retail_price', 'final_price_gbp_pence': 'final_price'}, inplace=True)
 

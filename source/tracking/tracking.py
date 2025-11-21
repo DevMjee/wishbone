f5d69163f8128db964fa8a1bc359736266a51143
AdamJC20 <adamcummings20@googlemail.com>
2025-11-21 11:07:14 +0000

    added error for deleting nonexistent email

--- source/tracking/tracking.py
+++ source/tracking/tracking.py
@@ -42,12 +42,18 @@ def subscribe_to_game(game_id: int, email: str, conn: connection) -> dict:
 def remove_email(email: str, conn: connection) -> dict:
     delete_query = """
                     DELETE FROM wishbone."tracking"
-                    WHERE email = %s;
+                    WHERE email = %s
+                    RETURNING tracking_id;
                     """
 
     cur = conn.cursor()
 
     cur.execute(delete_query, (email,))
+
+    rows = cur.fetchall()
+
+    if rows == 0:
+        return {'status': 'error', 'msg': f"email '{email}' does not exist on the database"}
 
     conn.commit()
 

70ffe4d4309d61c6afb538d8868a9433ee3af7e2
AdamJC20 <adamcummings20@googlemail.com>
2025-11-24 21:26:24 +0000

    login system works

--- source/dashboard/app.py
+++ source/dashboard/app.py
@@ -6,62 +6,11 @@ from dotenv import load_dotenv
 import pandas as pd
 from psycopg2 import connect
 from psycopg2.extensions import connection
+from backend import check_login, hash_password, delete_user, new_user
 
 load_dotenv()
 
 LOGO_IMG_PATH = "./wishbone_logo.png"
-
-
-def hash_password(password: str) -> bytes:
-    password_bytes = bytes(password, encoding='utf-8')
-    salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password_bytes, salt)
-
-
-def new_user(username: str, password: str, conn: connection):
-    hashed_password = hash_password(password)
-
-    insert_query = """
-                    INSERT INTO wishbone."login"
-                    (username, password_hash)
-                    VALUES
-                        (%s, %s)
-                    """
-
-    cur = conn.cursor()
-
-    try:
-        cur.execute(insert_query, (username, hashed_password))
-    except:
-        print("Something went wrong creating new user")
-
-    conn.commit()
-
-    cur.close()
-
-
-def check_login(username: str, password: bytes, conn: connection) -> bool:
-    check_query = f"""
-                SELECT *
-                FROM wishbone."login"
-                WHERE username = '{username}';
-                """
-
-    user_data = pd.read_sql_query(check_query, conn)
-
-    if user_data.empty:
-        return False
-
-    user_dict = user_data.to_dict('series')
-
-    print(f'USER DICT = {user_dict}')
-
-    if len(user_dict.get('login_id')) == 1:
-        stored_hash = bytes(user_dict.get(
-            'password_hash')[0], encoding='utf-8')
-        return bcrypt.checkpw(password, stored_hash)
-
-    raise LookupError('Something dreadful has happened')
 
 
 def get_connection() -> connection:
@@ -143,8 +92,10 @@ def create_current_price_metrics() -> None:
             pressed_login = st.button(label='Login')
             if (pressed_login) and (username) and (password):
                 login = check_login(username, password, db_conn)
+
                 if login:
-                    st.text('yes')  # USER IS LOGGED IN, DO LOGGED IN THINGS
+                    # TODO USER IS LOGGED IN, DO LOGGED IN THINGS
+                    st.text('yes')
 
                 else:
                     st.text('Username or password is incorrect')
--- source/dashboard/backend.py
+++ source/dashboard/backend.py
@@ -1,6 +1,75 @@
 import aioboto3
 import json
 import asyncio
+from psycopg2 import connect
+from psycopg2.extensions import connection
+import bcrypt
+import pandas as pd
+
+
+def check_login(username: str, password: bytes, conn: connection) -> bool:
+    check_query = f"""
+                SELECT *
+                FROM wishbone."login"
+                WHERE username = '{username}';
+                """
+
+    user_data = pd.read_sql_query(check_query, conn)
+
+    if user_data.empty:
+        return False
+
+    user_dict = user_data.to_dict('series')
+
+    if len(user_dict.get('login_id')) == 1:
+        stored_hash = bytes(user_dict.get(
+            'password_hash')[0], encoding='utf-8')
+        return bcrypt.checkpw(password, stored_hash)
+
+    raise LookupError('Something dreadful has happened')
+
+
+def hash_password(password: str) -> bytes:
+    password_bytes = bytes(password, encoding='utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password_bytes, salt)
+
+
+def delete_user(username: str, conn: connection) -> None:
+    delete_query = f"""
+                    DELETE FROM wishbone."login"
+                    WHERE username = '{username}';
+                    """
+
+    cur = conn.cursor()
+
+    cur.execute(delete_query)
+
+    conn.commit()
+
+    cur.close()
+
+
+def new_user(username: str, password: str, conn: connection):
+    hashed_password = hash_password(password)
+
+    insert_query = """
+                    INSERT INTO wishbone."login"
+                    (username, password_hash)
+                    VALUES
+                        (%s, %s)
+                    """
+
+    cur = conn.cursor()
+
+    try:
+        cur.execute(insert_query, (username, hashed_password.decode()))
+    except:
+        print("Something went wrong creating new user")
+
+    conn.commit()
+
+    cur.close()
 
 
 async def trigger_lambda(payload: dict) -> dict:

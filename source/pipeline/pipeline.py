714a061da15bb2ab76ab30d6954d58de24c4e3e4
DevMjee <dev.mjee2@gmail.com>
2025-11-28 09:45:41 +0000

    adding new search and etl to lambda

--- source/pipeline/pipeline.py
+++ source/pipeline/pipeline.py
@@ -1,19 +1,15 @@
 """Lambda function for the transform and load stages"""
-from extract_gog import extract_gog
-from extract_steam import export_steam
+from extract import extract_games
 from transform import transform_all
 from load import load_data
 
 
 def lambda_handler(event, context):
     try:
-        extract_gog()
+        game_inputs = event.get('game_inputs')
+        extract_games(game_inputs)
     except Exception as e:
         return {'status': 'error', 'msg': f'{str(e)} occurred in extract_gog'}
-    try:
-        export_steam()
-    except Exception as e:
-        return {'status': 'error', 'msg': f'{str(e)} occurred in extract_steam'}
     try:
         transform_all()
     except Exception as e:
